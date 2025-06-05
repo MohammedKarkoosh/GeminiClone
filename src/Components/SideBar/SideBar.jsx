3a54af8e0dd41e984e6e833df71d1c2a3aa67892
MohammedKarkoosh <mohammedkakkroosh@gmail.com>
2025-06-04 23:05:15 -0500

    Made the sidebar open and close when clicking on the open sidebar button

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -5,40 +5,69 @@ import { FiMessageSquare } from "react-icons/fi";
 import { MdOutlineQuestionMark } from "react-icons/md";
 import { FaHistory } from "react-icons/fa";
 import { IoSettingsOutline } from "react-icons/io5";
+import { useState } from "react";
 
 function SideBar() {
+
+  const [openSideBar, setOpenSideBar]= useState(false);
+  
+
+
   return (
     <div className="side-bar">
       <div className="menu">
-        <CiMenuBurger className="menu-burger" size={"20px"}/>
-
+        <div className="menu-burger">
+        <CiMenuBurger className="menu-burger-icon" size={"20px"}
+        onClick={()=>setOpenSideBar(prev=>!prev)}
+        />
+        </div>
         <div className="new-chat">
           <LuCircleFadingPlus size={"20px"}/>
-          <p>Start a New Chat</p>
+          {
+            openSideBar ?
+            <p>Start a New Chat</p>
+            :
+            null
+          }
+          
         </div>
+
+          {
+            openSideBar ?
 
         <div className="recent-chats">
           <p className="recent-title">Recent</p>
           <div className="recents-chats-title">
-            <FiMessageSquare />
+            <FiMessageSquare  size={"20px"}/>
             <p>Lorem ipsum dolor sit</p>
           </div>
         </div>
+          : null
+          }
       </div>
       <div className="menu-footer">
         <div className="menu-footer-item recents-chats-title">
-          <MdOutlineQuestionMark />
-          <p>Help</p>
+          <MdOutlineQuestionMark  size={"20px"}/>
+           {
+            openSideBar ? <p>Help</p>
+
+            :null 
+           }
         </div>
 
         <div className="menu-footer-item recents-chats-title">
-          <FaHistory />
-          <p>Past Activity</p>
+          <FaHistory  size={"20px"}/>
+          {
+            openSideBar ? <p>Past Activity</p>
+
+            :null}
         </div>
 
         <div className="menu-footer-item recents-chats-title">
-          <IoSettingsOutline />
-          <p>Settings</p>
+          <IoSettingsOutline size={"20px"}/>
+           {
+            openSideBar ?<p>Settings</p>
+            :null}
         </div>
       </div>
     </div>
